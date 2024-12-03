e918fd2a0f860a28ad2277188ddd47b2e9fa291c
Rojen Dangol <rojendangol44@gmail.com>
2024-12-03 11:43:03 +0545

    N+1 solver

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -16,9 +17,10 @@ class AppServiceProvider extends ServiceProvider
 
     /**
      * Bootstrap any application services.
+     * gets triggeried after all the dependencies have been loaded.
      */
     public function boot(): void
     {
-        //
+        Model::preventLazyLoading();
     }
 }
